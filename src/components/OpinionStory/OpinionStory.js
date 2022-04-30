06e6520661d95a079af1f27af331326772e7c240
Loic Verrall <loic.verrall@gmail.com>
2022-04-30 17:19:35 +0100

    Align avatar images right on mobile and laptop

--- src/components/OpinionStory/OpinionStory.js
+++ src/components/OpinionStory/OpinionStory.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components/macro';
+import { QUERIES } from '../../constants';
 
 const OpinionStory = ({ id, title, author, avatar, last }) => {
   return (
@@ -19,6 +20,8 @@ const Wrapper = styled.article`
   color: var(--color-gray-900);
   border-bottom: ${props => props.last ? 'none' : '2px solid var(--color-gray-300)'};
   padding: 16px 0;
+  display: grid;
+  grid-template-columns: auto 50px;
 `;
 
 const Avatar = styled.img`
@@ -27,6 +30,12 @@ const Avatar = styled.img`
   height: 48px;
   border-radius: 50%;
   object-fit: cover;
+  grid-column: 2 / 3;
+  grid-row: 1 / 4;
+
+  @media${QUERIES.tabletOnly} {
+    grid-column: 1 / 3;
+  }
 `;
 
 const AuthorName = styled.p`
@@ -34,12 +43,14 @@ const AuthorName = styled.p`
   font-weight: var(--font-weight-medium);
   color: var(--color-gray-700);
   margin-bottom: 4px;
+  grid-column: 1 / 3;
 `;
 
 const ArticleTitle = styled.h3`
   font-size: 1.125rem;
   font-weight: var(--font-weight-bold);
   line-height: 1.3;
+  grid-column: 1 / 3;
 `;
 
 export default OpinionStory;
