c2e8cd336967c08d9bfde4c48e0c29cdd5e83ac0
Loic Verrall <loic.verrall@gmail.com>
2022-04-30 16:22:46 +0100

    Set header styles for tablet and laptop

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -29,7 +29,24 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <ActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
         <Logo />
+
+        <VerticalStack>
+          <Button>
+            Subscribe
+          </Button>
+          <a>
+            Already a subscriber?
+          </a>
+        </VerticalStack>
       </MainHeader>
     </header>
   );
@@ -39,6 +56,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -50,6 +71,10 @@ const ActionGroup = styled.div`
   display: flex;
   gap: 24px;
 
+  @media${QUERIES.laptopAndUp} {
+    color: var(--color-gray-900);
+  }
+
   /*
     FIX: Remove the inline spacing that comes with
     react-feather icons.
@@ -59,12 +84,44 @@ const ActionGroup = styled.div`
   }
 `;
 
+const VerticalStack = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 24px;
+  align-items: end;
+
+  a {
+    font-style: italic;
+    text-decoration: underline;
+    margin-top: 1em;
+    color: var(--color-gray-900);
+  }
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+
+  ${ActionGroup}, ${VerticalStack} {
+    display: none;
+  }
+
+  @media${QUERIES.tabletAndUp} {
+    margin-top: 60px;
+    margin-bottom: 70px;
+  }
+
+  @media${QUERIES.laptopAndUp} {
+    margin-top: 0;
+    justify-content: space-between;
+
+    ${ActionGroup}, ${VerticalStack} {
+      display: flex;
+    }
+  }
 `;
 
 export default Header;
